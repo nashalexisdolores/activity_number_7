2c78716462182c748d96a8abcc15382db748c612
nashalexisdolores <alxsdlrs@gmail.com>
2026-05-30 23:00:04 +0800

    Add getter and setter for color para maangas

--- activity_7_fan.py
+++ activity_7_fan.py
@@ -26,3 +26,9 @@ class Fan:
 
     def set_radius(self, radius):
         self.__radius = radius
+
+    def get_color(self):
+        return self.__color
+
+    def set_color(self, color):
+        self.__color = color
