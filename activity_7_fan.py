54ef21641cc73c60fd921d3bc5cae0301bc2f479
nashalexisdolores <alxsdlrs@gmail.com>
2026-05-30 23:27:56 +0800

    I replaced the code because there is an error

--- activity_7_fan.py
+++ activity_7_fan.py
@@ -1,9 +1,10 @@
-class Fan:
+class fan_class:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
 
     def __init__(self, speed=SLOW, radius=5, color="blue", on=False):
+        # COMMIT 3: Private fields using snake_case
         self.__speed = speed
         self.__radius = radius
         self.__color = color
@@ -12,23 +13,23 @@ class Fan:
     def get_speed(self):
         return self.__speed
 
-    def get_radius(self):
-        return self.__radius
-
-    def get_color(self):
-        return self.__color
-
-    def is_on(self):
-        return self.__on
-
     def set_speed(self, speed):
         self.__speed = speed
+
+    def get_radius(self):
+        return self.__radius
 
     def set_radius(self, radius):
         self.__radius = radius
 
+    def get_color(self):
+        return self.__color
+
     def set_color(self, color):
         self.__color = color
+r
+    def is_on(self):
+        return self.__on
 
     def set_on(self, on):
         self.__on = on
